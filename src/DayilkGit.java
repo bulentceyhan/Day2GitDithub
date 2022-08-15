a03dd12f0343f321c6a304dfa080118e47d0c4a8
BulentCeyhan <ceyhanbulent@gmail.com>
2022-08-15 20:02:26 +0200

    v2

--- src/DayilkGit.java
+++ src/DayilkGit.java
@@ -1,5 +1,6 @@
 public class DayilkGit {
     public static void main(String[] args) {
         System.out.println("Git birinci deneme");
+        System.out.println("Hello Git 2. deneme")
     }
 }
